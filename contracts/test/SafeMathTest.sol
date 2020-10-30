195825e9ac18bdb26579cee015d6b2a9aefc05e0
dapp8coder <dapp.coder@protonmail.com>
2020-10-30 18:47:40 +0800

    uint128

--- contracts/test/SafeMathTest.sol
+++ contracts/test/SafeMathTest.sol
@@ -76,165 +76,66 @@ contract SafeMathTest256 {
 contract SafeMathTest128 {
     using SafeMath for uint128;
     function max(uint128 a_, uint128 b_) public pure returns (uint128) {
-        return a_.max(b_);
+        return uint128(uint256(a_).max(b_));
     } 
     function min(uint128 a_, uint128 b_) public pure returns (uint128) {
-        return a_.min(b_);
+        return uint128(uint256(a_).min(b_));
     } 
     function twins(uint128 a_, uint128 b_) public pure returns (uint128[2] memory) {
-        return a_.twins(b_);
+        return uint128(uint256(a_).twins(b_)); 
     }
     function avg(uint128 a_, uint128 b_) public pure returns (uint128) {
-        return a_.avg(b_);
+        return uint128(uint256(a_).avg(b_)); 
     }
     function add(uint128 a_, uint128 b_) public pure returns (uint128) { 
-        return a_.add(b_);
+        return uint128(uint256(a_).add(b_));  
     }
     function sub(uint128 a_, uint128 b_) public pure returns (uint128) {
-        return a_.sub(b_);
+        return uint128(uint256(a_).sub(b_));   
     }
     function mul(uint128 a_, uint128 b_) public pure returns (uint128) {
-        return a_.mul(b_);
+        return uint128(uint256(a_).mul(b_));   
     }
     function div(uint128 a_, uint128 b_) public pure returns (uint128) {
-        return a_.div(b_);
+        return uint128(uint256(a_).div(b_));   
     }  
     function mod(uint128 a_, uint128 b_) public pure returns (uint128) {
-        return a_.mod(b_);
+        return uint128(uint256(a_).mod(b_));   
     } 
     function sqrt(uint128 a_) public pure returns (uint128) {
-        return a_.sqrt();
+        return uint128(uint256(a_).sqrt());  
     }
 }
 contract SafeMathTest64 {
     using SafeMath for uint64;
     function max(uint64 a_, uint64 b_) public pure returns (uint64) {
-        return a_.max(b_);
+        return uint64(uint256(a_).max(b_));
     } 
     function min(uint64 a_, uint64 b_) public pure returns (uint64) {
-        return a_.min(b_);
+        return uint64(uint256(a_).min(b_));
     } 
     function twins(uint64 a_, uint64 b_) public pure returns (uint64[2] memory) {
-        return a_.twins(b_);
+        return uint64(uint256(a_).twins(b_)); 
     }
     function avg(uint64 a_, uint64 b_) public pure returns (uint64) {
-        return a_.avg(b_);
+        return uint64(uint256(a_).avg(b_)); 
     }
     function add(uint64 a_, uint64 b_) public pure returns (uint64) { 
-        return a_.add(b_);
+        return uint64(uint256(a_).add(b_));  
     }
     function sub(uint64 a_, uint64 b_) public pure returns (uint64) {
-        return a_.sub(b_);
+        return uint64(uint256(a_).sub(b_));   
     }
     function mul(uint64 a_, uint64 b_) public pure returns (uint64) {
-        return a_.mul(b_);
+        return uint64(uint256(a_).mul(b_));   
     }
     function div(uint64 a_, uint64 b_) public pure returns (uint64) {
-        return a_.div(b_);
+        return uint64(uint256(a_).div(b_));   
     }  
     function mod(uint64 a_, uint64 b_) public pure returns (uint64) {
-        return a_.mod(b_);
+        return uint64(uint256(a_).mod(b_));   
     } 
     function sqrt(uint64 a_) public pure returns (uint64) {
-        return a_.sqrt();
+        return uint64(uint256(a_).sqrt());  
     }
-}
-contract SafeMathTest32 {
-    using SafeMath for uint32;
-    function max(uint32 a_, uint32 b_) public pure returns (uint32) {
-        return a_.max(b_);
-    } 
-    function min(uint32 a_, uint32 b_) public pure returns (uint32) {
-        return a_.min(b_);
-    } 
-    function twins(uint32 a_, uint32 b_) public pure returns (uint32[2] memory) {
-        return a_.twins(b_);
-    }
-    function avg(uint32 a_, uint32 b_) public pure returns (uint32) {
-        return a_.avg(b_);
-    }
-    function add(uint32 a_, uint32 b_) public pure returns (uint32) { 
-        return a_.add(b_);
-    }
-    function sub(uint32 a_, uint32 b_) public pure returns (uint32) {
-        return a_.sub(b_);
-    }
-    function mul(uint32 a_, uint32 b_) public pure returns (uint32) {
-        return a_.mul(b_);
-    }
-    function div(uint32 a_, uint32 b_) public pure returns (uint32) {
-        return a_.div(b_);
-    }  
-    function mod(uint32 a_, uint32 b_) public pure returns (uint32) {
-        return a_.mod(b_);
-    } 
-    function sqrt(uint32 a_) public pure returns (uint32) {
-        return a_.sqrt();
-    }
-}
-contract SafeMathTest16 {
-    using SafeMath for uint16;
-    function max(uint16 a_, uint16 b_) public pure returns (uint16) {
-        return a_.max(b_);
-    } 
-    function min(uint16 a_, uint16 b_) public pure returns (uint16) {
-        return a_.min(b_);
-    } 
-    function twins(uint16 a_, uint16 b_) public pure returns (uint16[2] memory) {
-        return a_.twins(b_);
-    }
-    function avg(uint16 a_, uint16 b_) public pure returns (uint16) {
-        return a_.avg(b_);
-    }
-    function add(uint16 a_, uint16 b_) public pure returns (uint16) { 
-        return a_.add(b_);
-    }
-    function sub(uint16 a_, uint16 b_) public pure returns (uint16) {
-        return a_.sub(b_);
-    }
-    function mul(uint16 a_, uint16 b_) public pure returns (uint16) {
-        return a_.mul(b_);
-    }
-    function div(uint16 a_, uint16 b_) public pure returns (uint16) {
-        return a_.div(b_);
-    }  
-    function mod(uint16 a_, uint16 b_) public pure returns (uint16) {
-        return a_.mod(b_);
-    } 
-    function sqrt(uint16 a_) public pure returns (uint16) {
-        return a_.sqrt();
-    }
-}
-contract SafeMathTest8 {
-    using SafeMath for uint8;
-    function max(uint8 a_, uint8 b_) public pure returns (uint8) {
-        return a_.max(b_);
-    } 
-    function min(uint8 a_, uint8 b_) public pure returns (uint8) {
-        return a_.min(b_);
-    } 
-    function twins(uint8 a_, uint8 b_) public pure returns (uint8[2] memory) {
-        return a_.twins(b_);
-    }
-    function avg(uint8 a_, uint8 b_) public pure returns (uint8) {
-        return a_.avg(b_);
-    }
-    function add(uint8 a_, uint8 b_) public pure returns (uint8) { 
-        return a_.add(b_);
-    }
-    function sub(uint8 a_, uint8 b_) public pure returns (uint8) {
-        return a_.sub(b_);
-    }
-    function mul(uint8 a_, uint8 b_) public pure returns (uint8) {
-        return a_.mul(b_);
-    }
-    function div(uint8 a_, uint8 b_) public pure returns (uint8) {
-        return a_.div(b_);
-    }  
-    function mod(uint8 a_, uint8 b_) public pure returns (uint8) {
-        return a_.mod(b_);
-    } 
-    function sqrt(uint8 a_) public pure returns (uint8) {
-        return a_.sqrt();
-    }
-}
+}  
