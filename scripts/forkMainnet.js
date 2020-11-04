66b2c98d4f6d0990ebae707dc9e96e44b53d888d
dapp8coder <dapp.coder@protonmail.com>
2020-11-04 19:36:25 +0800

    sync

--- scripts/forkMainnet.js
+++ scripts/forkMainnet.js
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2020 Swap.Pet@pm.me
 // scripts/forkMainnet.js
-const { forkChainCMD } = require("./utils");
-forkChainCMD(); 
+const { forkChain } = require("./utils");
+
+const forkMainnet = async () => {
+  const { serverListen, serverClose } = await forkChain();
+  await serverListen(); 
+};
+forkMainnet(); 
