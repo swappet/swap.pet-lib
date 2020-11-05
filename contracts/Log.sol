170ee63076707d90de8cfc255b867ec3fdd366aa
dapp8coder <dapp.coder@protonmail.com>
2020-11-05 18:23:52 +0800

    add NoteLog with msg.value

--- contracts/Log.sol
+++ contracts/Log.sol
@@ -3,31 +3,26 @@
 // contracts/Log.sol
 pragma solidity ^0.7.0; 
 
-/// @notice log an 'anonymous' event with a constant 6 words of calldata
+/// @notice log an 'anonymous' event
 /// @dev    log with four indexed topics: selector, caller, arg1 and arg2
 abstract contract Log {
-    event Log(
-        bytes4   indexed  sig,          //  selector
-        address  indexed  usr,          //  caller
+    event NoteLog(
+        bytes4   indexed  sig,              //  selector:msg.sig
+        address  indexed  caller,           //  caller:msg.sender
         bytes32  indexed  arg1,
         bytes32  indexed  arg2,
-        bytes             data
+        uint              value,            //  msg.value
+        bytes             data              //  msg.data
     ) anonymous;
 
-    modifier log {
+    modifier note {
         _;
+        bytes32 arg1;
+        bytes32 arg2;
         assembly {
-            let mark := msize()                       // end of memory ensures zero
-            mstore(0x40, add(mark, 288))              // update free memory pointer
-            mstore(mark, 0x20)                        // bytes type data offset
-            mstore(add(mark, 0x20), 224)              // bytes size (padded)
-            calldatacopy(add(mark, 0x40), 0, 224)     // bytes payload
-            log4(mark, 288,                           // calldata
-                 shl(224, shr(224, calldataload(0))), // msg.sig
-                 caller(),                            // msg.sender
-                 calldataload(4),                     // arg1
-                 calldataload(36)                     // arg2
-                )
+            arg1 := calldataload(4)
+            arg2 := calldataload(36)
         }
+        NoteLog(msg.sig, msg.sender, arg1, arg2, msg.value, msg.data);
     }
 }
